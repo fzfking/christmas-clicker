0e524ae003c5d74e14a8b7ad5fe350e5ea51c1c5
fzfking <48185056+fzfking@users.noreply.github.com>
2024-12-31 02:38:49 +0300

    feat: serializer static service

---
+++ src/ChristmasClicker/Assets/Scripts/Common/Services/SerializerStaticService.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace Common.Services
+{
+    public class SerializerStaticService
+    {
+        private static readonly JsonSerializerSettings JsonSettings = new()
+        {
+            TypeNameHandling = TypeNameHandling.Auto,
+            Formatting = Formatting.Indented
+        };
+        
+        public static string Serialize<T>(T value)
+        {
+            return JsonConvert.SerializeObject(value, JsonSettings);
+        }
+
+        public static T Deserialize<T>(string value)
+        {
+            return JsonConvert.DeserializeObject<T>(value, JsonSettings);
+        }
+    }
+}
